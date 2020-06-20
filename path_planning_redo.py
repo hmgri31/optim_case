6645c5ec7eea12dd92df5369970b6dfa13354d10
DESKTOP-TE30666\Hayden <hmgri31@gmail.com>
2020-06-20 18:04:32 +0200

    Adding code cells

--- path_planning_redo.py
+++ path_planning_redo.py
@@ -16,11 +16,11 @@ L = 1.5 #Define what the length of the car is, as this will affect the turning c
 
 
 Nsim    = 30            # how much samples to simulate
-nx = 4 # x, y, v, theta (angle bicycle)
-nu = 2 # a, delta (angle wheel)
-Tf = 5 # Control horizon [s]
-Nhor = 40 #number of control intervals
-dt = Tf/Nhor #sample time
+nx = 4                  # x, y, v, theta (angle bicycle)
+nu = 2                  # a, delta (angle wheel)
+Tf = 5                  # Control horizon [s]
+Nhor = 10              #number of control intervals
+dt = Tf/Nhor            #sample time
 
 time_hist      = np.zeros((Nsim+1, Nhor+1))
 x_hist         = np.zeros((Nsim+1, Nhor+1))
@@ -34,10 +34,10 @@ ocp = Ocp(T=FreeTime(10.0))
 # Define states
 x     = ocp.state()
 y     = ocp.state()
-theta = ocp.state()
 v     = ocp.state()
+theta = ocp.state()
 
-# Defince controls
+# Define controls
 delta = ocp.control()
 a     = ocp.control()
 
@@ -45,9 +45,10 @@ a     = ocp.control()
 ocp.set_der(x,      v*cos(theta))
 ocp.set_der(y,      v*sin(theta))
 ocp.set_der(theta,  v/L*tan(delta))
+ocp.set_der(v,      a)
 
 # Define parameter
-X_0 = ocp.parameter(nx,1)
+X_0 = ocp.parameter(nx)
 
 # Initial constraints
 X = vertcat(x, y, v, theta)
@@ -64,17 +65,17 @@ ocp.set_initial(a,      0.5)
 ocp.set_initial(delta,  0)
 
 # Path constraints
-ocp.subject_to( 0 <= (v <= 10))
-ocp.subject_to( -2 <= (x<= 12))
-ocp.subject_to( -2 <= (y<= 12))
+ocp.subject_to(0 <= (v <= 10))
+ocp.subject_to(-2 <= (x <= 12))
+ocp.subject_to(-2 <= (y <= 12))
 
-ocp.subject_to( -10 <= (a <= 10))
-ocp.subject_to( -pi/6 <= (delta <= pi/6) )
+ocp.subject_to(-10 <= (a <= 10))
+ocp.subject_to(-pi/6 <= (delta <= pi/6))
 
-# Objective 
+# Objective functions
 ocp.add_objective(sumsqr(ocp.T))
-ocp.add_objective(sumsqr(a))
-ocp.add_objective(sumsqr(delta))
+#ocp.add_objective(sumsqr(a))
+#ocp.add_objective(sumsqr(delta))
 #ocp.add_objective(-sumsqr(v))
 
 # Pick a solution method
@@ -87,7 +88,11 @@ ocp.method(MultipleShooting(N=Nhor, M=1, intg='rk'))
 
 # Set initial value for states
 current_X = vertcat(0,0,0,pi/4)
-ocp.set_value(X_0, current_X)
+ocp.set_value(X_0,current_X)
+
+#Specify the end
+final_X = vertcat(10,10,0,pi/4)
+ocp.subject_to(ocp.at_tf(X)==final_X)
 
 sol = ocp.solve()
 
